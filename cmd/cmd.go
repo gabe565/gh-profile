0eb91f719104803e72b878f0b7ab5a435dc448b6
Gabe Cook <gabe565@gmail.com>
2022-10-11 12:58:34 -0500

    :sparkles: Prompt for profile if not given

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 	"os"
 	"path/filepath"
@@ -10,16 +11,11 @@ import (
 )
 
 var Command = &cobra.Command{
-	Use:  "profile name",
+	Use:  "profile [name]",
 	RunE: run,
 }
 
 func run(cmd *cobra.Command, args []string) error {
-	if len(args) < 1 {
-		return errors.New("profile name is required")
-	}
-	profile := args[0]
-
 	conf, err := getConfigDir()
 	if err != nil {
 		return err
@@ -36,6 +32,18 @@ func run(cmd *cobra.Command, args []string) error {
 	} else if !isLink {
 		fmt.Println(`Naming existing profile to default`)
 		if err := newDefaultProfile(); err != nil {
+			return err
+		}
+	}
+
+	cmd.SilenceUsage = true
+
+	var profile string
+	if len(args) > 0 {
+		profile = args[0]
+	} else {
+		profile, err = promptProfile()
+		if err != nil {
 			return err
 		}
 	}
@@ -129,4 +137,33 @@ func switchProfile(name string) error {
 	}
 
 	return nil
+}
+
+func promptProfile() (string, error) {
+	conf, err := getConfigDir()
+	if err != nil {
+		return "", err
+	}
+
+	files, err := os.ReadDir(filepath.Join(conf, "accounts"))
+	if err != nil {
+		return "", err
+	}
+
+	profiles := make([]string, 0, len(files))
+	for _, file := range files {
+		if file.IsDir() {
+			profiles = append(profiles, file.Name())
+		}
+	}
+
+	var profile string
+	if err := survey.AskOne(&survey.Select{
+		Message: "Choose a profile",
+		Options: profiles,
+	}, &profile, survey.WithValidator(survey.Required)); err != nil {
+		return "", err
+	}
+
+	return profile, nil
 }
--- go.mod
+++ go.mod
@@ -2,9 +2,19 @@ module github.com/gabe565/gh-profile
 
 go 1.19
 
-require github.com/spf13/cobra v1.6.0
+require (
+	github.com/AlecAivazis/survey/v2 v2.3.6
+	github.com/spf13/cobra v1.6.0
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/term v0.0.0-20210503060354-a79de5458b56 // indirect
+	golang.org/x/text v0.3.3 // indirect
 )
