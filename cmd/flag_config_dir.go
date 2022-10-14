7637db42bb001e2008836fd3d8b1b2417b665d33
Gabe Cook <gabe565@gmail.com>
2022-10-14 12:42:10 -0500

    :wrench: Use GH_CONFIG_DIR if set

--- cmd/flag_config_dir.go
+++ cmd/flag_config_dir.go
@@ -2,11 +2,15 @@ package cmd
 
 import (
 	"github.com/spf13/viper"
+	"os"
 	"path/filepath"
 )
 
 func init() {
-	defaultDir := filepath.Join("$HOME", ".config", "gh")
+	defaultDir := os.Getenv("GH_CONFIG_DIR")
+	if defaultDir == "" {
+		defaultDir = filepath.Join("$HOME", ".config", "gh")
+	}
 	Command.PersistentFlags().StringP("config-dir", "c", defaultDir, "GitHub CLI config dir")
 	if err := viper.BindPFlag("config-dir", Command.PersistentFlags().Lookup("config-dir")); err != nil {
 		panic(err)
