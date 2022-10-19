d185690edb673c8e2418cff2773ab5e6210658a1
Gabe Cook <gabe565@gmail.com>
2022-10-19 00:27:57 -0500

    :sparkles: Check XDG_CONFIG_HOME when building default config dir

--- cmd/flag_config_dir.go
+++ cmd/flag_config_dir.go
@@ -10,7 +10,11 @@ import (
 func flagConfigDir(cmd *cobra.Command) {
 	defaultDir := os.Getenv("GH_CONFIG_DIR")
 	if defaultDir == "" {
-		defaultDir = filepath.Join("$HOME", ".config", "gh")
+		if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
+			defaultDir = filepath.Join(xdgConfigHome, "gh")
+		} else {
+			defaultDir = filepath.Join("$HOME", ".config", "gh")
+		}
 	}
 	cmd.PersistentFlags().StringP("config-dir", "c", defaultDir, "GitHub CLI config dir")
 	if err := viper.BindPFlag("config-dir", cmd.PersistentFlags().Lookup("config-dir")); err != nil {
