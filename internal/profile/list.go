a30f7df9a3ff3f99f330034608737d9c5b995a24
Gabe Cook <gabe565@gmail.com>
2022-10-19 19:09:03 -0500

    :loud_sound: Output prettier error if no profiles exist

--- internal/profile/list.go
+++ internal/profile/list.go
@@ -1,16 +1,22 @@
 package profile
 
 import (
+	"errors"
 	"github.com/gabe565/gh-profile/internal/github"
 	"os"
 	"path/filepath"
 )
+
+var ErrNoProfiles = errors.New("no profiles found")
 
 func List() ([]Profile, error) {
 	conf := github.RootConfigDir()
 
 	files, err := os.ReadDir(filepath.Join(conf, "profiles"))
 	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			err = ErrNoProfiles
+		}
 		return []Profile{}, err
 	}
 
