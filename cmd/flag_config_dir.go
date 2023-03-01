3fb0108835ea14180b218884d95fdbde21527c72
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-03-01 16:42:36 +0900

    [update] windows support

--- cmd/flag_config_dir.go
+++ cmd/flag_config_dir.go
@@ -1,17 +1,21 @@
 package cmd
 
 import (
+	"os"
+	"path/filepath"
+	"runtime"
+
 	"github.com/gabe565/gh-profile/internal/github"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	"os"
-	"path/filepath"
 )
 
 func flagConfigDir(cmd *cobra.Command) {
 	defaultDir := os.Getenv("GH_CONFIG_DIR")
 	if defaultDir == "" {
-		if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
+		if runtime.GOOS == "windows" {
+			defaultDir = filepath.Join("$HOME", "AppData", "Roaming", "GitHub CLI")
+		} else if xdgConfigHome := os.Getenv("XDG_CONFIG_HOME"); xdgConfigHome != "" {
 			defaultDir = filepath.Join(xdgConfigHome, "gh")
 		} else {
 			defaultDir = filepath.Join("$HOME", ".config", "gh")
